443c1f18092f2235ac778af03c4a6f6d6a32ea04
videmor <videmor@gmail.com>
2015-02-20 14:43:38 -0500

    Initial Commit

---
+++ Tab Bar Center v2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tab Bar Center v2
+//
+//  Created by Jorge Crisóstomo Palacios on 2/20/15.
+//  Copyright (c) 2015 videmor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
